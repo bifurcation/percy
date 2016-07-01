a7b03be921097160f77ef0e681c7bfc72839ae57
Richard Barnes <rlb@ipv.sx>
2016-07-01 13:58:00 -0400

    DTLS support

--- mdd.go
+++ mdd.go
@@ -1,28 +1,134 @@
 package percy
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
 	"log"
 	"net"
+	"time"
 )
 
-func addrToString(addr *net.UDPAddr) string {
-	// XXX: Something fancier?
-	return addr.String()
+type dtlsSRTPPacketClass uint8
+
+const (
+	packetClassDTLS dtlsSRTPPacketClass = iota
+	packetClassSRTP
+	packetClassSTUN
+	packetClassUnknown
+)
+
+// https://tools.ietf.org/html/rfc5764#section-5.1.2
+func packetClass(msg []byte) dtlsSRTPPacketClass {
+	if len(msg) == 0 {
+		return packetClassUnknown
+	}
+
+	// XXX: We could do more validation that DTLS and SRTP
+	//      packets are well-formed
+	B := msg[0]
+	switch {
+	case 127 < B && B < 192:
+		return packetClassSRTP
+	case 19 < B && B < 64:
+		return packetClassDTLS
+	case B < 2:
+		return packetClassSTUN
+	default:
+		return packetClassUnknown
+	}
+}
+
+type packet struct {
+	addr *net.UDPAddr
+	msg  []byte
+}
+
+func addrToAssoc(addr *net.UDPAddr) AssociationID {
+	var assoc AssociationID
+	h := sha256.New()
+	h.Write([]byte(addr.String()))
+	sum := h.Sum(nil)
+	copy(assoc[:], sum[:16])
+	return assoc
+}
+
+func assocToString(assoc AssociationID) string {
+	return hex.EncodeToString(assoc[:])
+}
+
+func stringToAssoc(clientID string) (AssociationID, error) {
+	var assoc AssociationID
+	decoded, err := hex.DecodeString(clientID)
+	if err != nil {
+		return AssociationID{}, err
+	}
+	copy(assoc[:], decoded)
+	return assoc, nil
 }
 
 type MDD struct {
-	name     string
-	addr     *net.UDPAddr
-	conn     *net.UDPConn
-	clients  map[string]*net.UDPAddr
-	stopChan chan bool
+	name       string
+	addr       *net.UDPAddr
+	conn       *net.UDPConn
+	clients    map[string]*net.UDPAddr
+	stopChan   chan bool
+	doneChan   chan bool
+	packetChan chan packet
+	timeout    time.Duration
+
+	kmf      KMFTunnel
+	keys     *SRTPKeys
+	profile  ProtectionProfile
+	profiles []ProtectionProfile
+	// TODO add some mutexes
 }
 
-func NewMDD() *MDD {
+func NewMDD(kmf KMFTunnel) *MDD {
 	mdd := new(MDD)
 	mdd.name = "mdd"
 	mdd.clients = map[string]*net.UDPAddr{}
+	mdd.kmf = kmf
+	mdd.timeout = 10 * time.Millisecond
+
+	mdd.stopChan = make(chan bool)
+	mdd.doneChan = make(chan bool)
+	mdd.packetChan = make(chan packet)
+
+	// TODO Add some defaults
+	mdd.profiles = []ProtectionProfile{}
+
 	return mdd
+}
+
+func (mdd *MDD) handleDTLS(assocID AssociationID, msg []byte) {
+	// Rough check for ClientHello
+	ch := len(msg) >= 14 && msg[0] == 0x16 && msg[13] == 0x01
+
+	if ch {
+		mdd.kmf.SendWithProfiles(assocID, msg, mdd.profiles)
+	} else {
+		mdd.kmf.Send(assocID, msg)
+	}
+}
+
+func (mdd *MDD) handleSRTP(clientID string, msg []byte) {
+	if mdd.keys != nil && mdd.profile != 0 {
+		// XXX: Here's where you mess with the packet if you want to
+	}
+
+	// Send the packet out to all the clients except
+	// the one that sent it
+	for client, addr := range mdd.clients {
+		if client == clientID {
+			continue
+		}
+
+		_, err := mdd.conn.WriteToUDP(msg, addr)
+		if err != nil {
+			log.Println("Error forwarding packet")
+		}
+	}
 }
 
 func (mdd *MDD) Listen(port int) error {
@@ -34,58 +140,97 @@ func (mdd *MDD) Listen(port int) error {
 		return err
 	}
 
-	go func(mdd *MDD) {
+	mdd.packetChan = make(chan packet, 10)
+
+	go func(packetChan chan packet) {
 		buf := make([]byte, 2048)
 
 		for {
-			select {
-			case <-mdd.stopChan:
-				log.Println("Stopping MDD")
-			default:
-			}
-
 			n, addr, err := mdd.conn.ReadFromUDP(buf)
 
+			if err == nil {
+				packetChan <- packet{addr: addr, msg: buf[:n]}
+			}
+			// TODO log errors
+		}
+	}(mdd.packetChan)
+
+	go func(mdd *MDD) {
+		for {
+			var pkt packet
+
+			select {
+			case <-mdd.stopChan:
+				mdd.doneChan <- true
+				return
+			case <-time.After(mdd.timeout):
+				continue
+			case pkt = <-mdd.packetChan:
+			}
+
 			if err != nil {
-				log.Printf("Recv Error: %v\n", err)
+				log.Printf("Recv Error: %v", err)
 				continue
 			}
 
-			log.Printf("Recv [%s] [%d] [%s] [%s]", mdd.name, n, addr.String(), string(buf[:n]))
+			assocID := addrToAssoc(pkt.addr)
+			clientID := assocToString(assocID)
 
 			// Remember the client if it's new
-			// If we can't make a conn to it, don't forward the packet
 			// XXX: Could have an interface to add/remove clients, then
 			//      just filter unknown clients here.
-			clientID := addrToString(addr)
 			if _, ok := mdd.clients[clientID]; !ok {
-				mdd.clients[clientID] = addr
+				mdd.clients[clientID] = pkt.addr
 			}
 
-			// TODO: if (dtlsPacket) { mdd.tunnel.send(buf) }
-
-			// XXX: Here's where you mess with the packet
-
-			// Send the packet out to all the clients except
-			// the one that sent it
-			for client, addr := range mdd.clients {
-				if client == clientID {
-					continue
-				}
-
-				_, err := mdd.conn.WriteToUDP(buf[:n], addr)
-				if err != nil {
-					log.Println("Error forwarding packet")
-				}
-				log.Printf("Send [%s] [%d] [%s] [%s]", mdd.name, n, client, string(buf[:n]))
+			switch packetClass(pkt.msg) {
+			case packetClassDTLS:
+				mdd.handleDTLS(assocID, pkt.msg)
+			case packetClassSRTP:
+				mdd.handleSRTP(clientID, pkt.msg)
+			case packetClassSTUN:
+				fallthrough
+			default:
+				log.Printf("Unknown packet type received")
 			}
 		}
 	}(mdd)
 
-	log.Println("Started MDD")
 	return nil
+}
+
+func (mdd *MDD) Send(assoc AssociationID, msg []byte) error {
+	clientID := assocToString(assoc)
+
+	if packetClass(msg) != packetClassDTLS {
+		return fmt.Errorf("Send called with non-DTLS packet")
+	}
+
+	addr, ok := mdd.clients[clientID]
+	if !ok {
+		return fmt.Errorf("Unknown client [%s]", clientID)
+	}
+
+	_, err := mdd.conn.WriteToUDP(msg, addr)
+	return err
+}
+
+func (mdd *MDD) SendWithKeys(assoc AssociationID, msg []byte, profile ProtectionProfile, keys SRTPKeys) error {
+	if packetClass(msg) != packetClassDTLS {
+		return fmt.Errorf("Send called with non-DTLS packet")
+	}
+
+	mdd.profile = profile
+	mdd.keys = &keys
+	return mdd.Send(assoc, msg)
 }
 
 func (mdd *MDD) Stop() {
 	mdd.stopChan <- true
+	<-mdd.doneChan
+
+	mdd.conn.Close()
+
+	// Avoid race conditions
+	<-time.After(10 * time.Millisecond)
 }
--- mdd_test.go
+++ mdd_test.go
@@ -3,9 +3,9 @@ package percy
 import (
 	"bytes"
 	"fmt"
-	"log"
 	"net"
 	"runtime"
+	"strconv"
 	"strings"
 	"testing"
 	"time"
@@ -17,8 +17,10 @@ type Client struct {
 	name       string
 	clientAddr *net.UDPAddr
 	serverAddr *net.UDPAddr
-	serverConn *net.UDPConn
+	conn       *net.UDPConn
 	stopChan   chan bool
+	doneChan   chan bool
+	packetChan chan packet
 }
 
 func NewClient(name string, clientPort, serverPort int) (*Client, error) {
@@ -28,8 +30,11 @@ func NewClient(name string, clientPort, serverPort int) (*Client, error) {
 	c.clientAddr = &net.UDPAddr{Port: clientPort}
 	c.serverAddr = &net.UDPAddr{Port: serverPort}
 
+	c.stopChan = make(chan bool, 1)
+	c.doneChan = make(chan bool, 1)
+
 	var err error
-	c.serverConn, err = net.DialUDP("udp", c.clientAddr, c.serverAddr)
+	c.conn, err = net.DialUDP("udp", c.clientAddr, c.serverAddr)
 	if err != nil {
 		return nil, err
 	}
@@ -38,24 +43,33 @@ func NewClient(name string, clientPort, serverPort int) (*Client, error) {
 }
 
 func (c *Client) Listen() error {
-	go func(c *Client) {
+
+	c.packetChan = make(chan packet, 10)
+
+	go func(packetChan chan packet) {
 		buf := make([]byte, 2048)
 
 		for {
+			n, addr, err := c.conn.ReadFromUDP(buf)
+
+			if err == nil {
+				packetChan <- packet{addr: addr, msg: buf[:n]}
+			}
+			// TODO log errors
+		}
+	}(c.packetChan)
+
+	go func(c *Client) {
+		for {
+			var pkt packet
+
 			select {
 			case <-c.stopChan:
-				log.Println("Stopping Client")
-			default:
-			}
-
-			n, addr, err := c.serverConn.ReadFromUDP(buf)
-
-			if err != nil {
-				log.Printf("Recv Error: %v\n", err)
-				continue
-			} else {
-				log.Printf("Recv [%s] [%d] [%s] [%s]", c.name, n, addr.String(), string(buf[:n]))
-				c.RecvChan <- buf[:n]
+				c.doneChan <- true
+				return
+			case pkt = <-c.packetChan:
+				c.RecvChan <- pkt.msg
+			case <-time.After(pause):
 			}
 		}
 	}(c)
@@ -64,12 +78,29 @@ func (c *Client) Listen() error {
 }
 
 func (c *Client) Write(msg []byte) error {
-	_, err := c.serverConn.Write(msg)
+	_, err := c.conn.Write(msg)
 	return err
 }
 
 func (c *Client) Stop() {
 	c.stopChan <- true
+	<-c.doneChan
+	c.conn.Close()
+}
+
+type CountTunnel struct {
+	sendCount             int
+	sendWithProfilesCount int
+}
+
+func (tun *CountTunnel) Send(assoc AssociationID, msg []byte) error {
+	tun.sendCount += 1
+	return nil
+}
+
+func (tun *CountTunnel) SendWithProfiles(assoc AssociationID, msg []byte, profiles []ProtectionProfile) error {
+	tun.sendWithProfilesCount += 1
+	return nil
 }
 
 /////
@@ -81,8 +112,8 @@ func caller() string {
 	return fmt.Sprintf("%s:%d:", filename, line)
 }
 
-func Assert(t *testing.T, result bool, message string) {
-	if !result {
+func AssertNotNil(t *testing.T, obj interface{}, message string) {
+	if obj == nil {
 		t.Fatalf("%s %s", caller(), message)
 	}
 }
@@ -93,44 +124,212 @@ func AssertNotError(t *testing.T, err error, message string) {
 	}
 }
 
-const packetTimeout = 100 * time.Millisecond
+func AssertEquals(t *testing.T, one interface{}, two interface{}) {
+	if one != two {
+		t.Fatalf("%s [%v] != [%v]", caller(), one, two)
+	}
+}
+
+const packetTimeout = 1000 * time.Millisecond
 
 func AssertRecvPacket(t *testing.T, c *Client, msg []byte, message string) {
 	select {
 	case packet := <-c.RecvChan:
-		if !bytes.Equal(msg, packet) {
-			t.Fatalf("%s %s: [%x] != [%x]", caller(), message, msg, packet)
+		if cmp := bytes.Compare(msg, packet); cmp != 0 {
+			t.Fatalf("%s %s: [%x] != [%x] ~ [%d]", caller(), message, msg, packet, cmp)
 		}
 	case <-time.After(packetTimeout):
 		t.Fatalf("%s %s: %s", caller(), message, "timeout")
 	}
 }
 
+func AssertNotRecvPacket(t *testing.T, c *Client, message string) {
+	select {
+	case <-c.RecvChan:
+		t.Fatalf("%s %s: %s", caller(), "Should not have received a packet")
+	case <-time.After(packetTimeout):
+	}
+}
+
 /////
+
+var (
+	// XXX: If you make nClients too big, you run into race
+	// conditions with the n^2 tests
+	nClients       = 10
+	pause          = 10 * time.Millisecond
+	serverPort     = 8888
+	clientBasePort = 9999
+)
 
 func TestRTPForwarding(t *testing.T) {
 	// Set up the server
-	mdd := NewMDD()
-	err := mdd.Listen(8888)
+	mdd := NewMDD(&CountTunnel{})
+	err := mdd.Listen(serverPort)
 	AssertNotError(t, err, "Error creating MDD")
+	defer mdd.Stop()
+
+	// Start up a bunch of clients
+	clients := make([]*Client, nClients)
+	for i := range clients {
+		port := 9999 + i
+		clients[i], err = NewClient("c"+strconv.Itoa(i), port, serverPort)
+		AssertNotError(t, err, fmt.Sprintf("Error creating client %d", i))
+		clients[i].Listen()
+		defer clients[i].Stop()
+	}
+
+	// Verify that previous clients hear new clients when they join
+	for i, sender := range clients {
+		srtpPacket := []byte{128, 0, byte(i)}
+		sender.Write(srtpPacket)
+
+		for j := 0; j < i; j += 1 {
+			AssertRecvPacket(t, clients[j], srtpPacket,
+				fmt.Sprintf("Failed to forward c%d->c%d", i, j))
+		}
+	}
+
+	// Verify that packets from joined clients broadcast to everyone
+	// but the sender
+	for i, sender := range clients {
+		srtpPacket := []byte{128, 1, byte(i)}
+		sender.Write(srtpPacket)
+
+		<-time.After(10 * time.Millisecond)
+
+		for j := 0; j < i; j += 1 {
+			if i == j {
+				AssertNotRecvPacket(t, clients[j], "Loopback detected")
+				continue
+			}
+
+			AssertRecvPacket(t, clients[j], srtpPacket,
+				fmt.Sprintf("Failed to broadcast c%d->c%d", i, j))
+		}
+	}
+}
+
+func TestDTLSForwarding(t *testing.T) {
+	// Set up the server and a client
+	tun := &CountTunnel{sendCount: 0, sendWithProfilesCount: 0}
+	mdd := NewMDD(tun)
+	err := mdd.Listen(serverPort)
+	AssertNotError(t, err, "Error creating MDD")
+	defer mdd.Stop()
+
+	client, err := NewClient("cDTLS", clientBasePort, serverPort)
+	AssertNotError(t, err, "Error creating Client")
+	client.Listen()
+	defer client.Stop()
+
+	// Test forward direction (ClientHello)
+	dtlsPacket := bytes.Repeat([]byte{0}, 14)
+	dtlsPacket[0] = 0x16
+	dtlsPacket[13] = 0x01
+	err = client.Write(dtlsPacket)
+	AssertNotError(t, err, "Error sending DTLS packet")
+	<-time.After(pause)
+	AssertEquals(t, tun.sendWithProfilesCount, 1)
+
+	// Test forward direction (non-ClientHello)
+	dtlsPacket = []byte{0x16, 0x00}
+	err = client.Write(dtlsPacket)
+	AssertNotError(t, err, "Error sending DTLS packet")
+	<-time.After(pause)
+	AssertEquals(t, tun.sendCount, 1)
+
+	// Grab the association ID for our client
+	var assoc AssociationID
+	for clientID := range mdd.clients {
+		assoc, err = stringToAssoc(clientID)
+		AssertNotError(t, err, "MDD provisioned an invalid client ID")
+	}
+
+	// Test reverse direction (no keys)
+	dtlsPacket = []byte{0x16, 0x01}
+	mdd.Send(assoc, dtlsPacket)
+	AssertRecvPacket(t, client, dtlsPacket, "DTLS packet was not forwarded")
+
+	// Test reverse direction (keys)
+	dtlsPacket = []byte{0x16, 0x02}
+	keys := SRTPKeys{}
+	profile := ProtectionProfile(0xff)
+	mdd.SendWithKeys(assoc, dtlsPacket, profile, keys)
+	AssertRecvPacket(t, client, dtlsPacket, "DTLS packet was not forwarded with keys")
+	AssertEquals(t, mdd.profile, profile)
+	AssertNotNil(t, mdd.keys, "MDD failed to set keys")
+}
+
+func TestDiscrimination(t *testing.T) {
+	// Set up the server
+	tun := &CountTunnel{sendCount: 0, sendWithProfilesCount: 0}
+	mdd := NewMDD(tun)
+	err := mdd.Listen(serverPort)
+	AssertNotError(t, err, "Error creating MDD")
+	defer mdd.Stop()
+
+	// Set up first client
+	client1, err := NewClient("cDTLS", clientBasePort, serverPort)
+	AssertNotError(t, err, "Error creating Client")
+	client1.Listen()
+	defer client1.Stop()
+
+	// Have client send a ClientHello and verify that it goes through
+	clientHelloPacket := bytes.Repeat([]byte{0}, 14)
+	clientHelloPacket[0] = 0x16
+	clientHelloPacket[13] = 0x01
+	err = client1.Write(clientHelloPacket)
+	<-time.After(pause)
+	AssertEquals(t, tun.sendWithProfilesCount, 1)
+	AssertEquals(t, tun.sendCount, 0)
+
+	// Grab the association ID for client 1
+	var client1ID string
+	var client1Assoc AssociationID
+	for clientID := range mdd.clients {
+		client1ID = clientID
+	}
+	client1Assoc, err = stringToAssoc(client1ID)
+	AssertNotError(t, err, "MDD provisioned an invalid client ID")
+
+	// Set up second client
+	client2, err := NewClient("cDTLS", clientBasePort+1, serverPort)
+	AssertNotError(t, err, "Error creating Client")
+	client2.Listen()
+	defer client2.Stop()
+
+	err = client2.Write(clientHelloPacket)
+	<-time.After(pause)
+	AssertEquals(t, tun.sendWithProfilesCount, 2)
+	AssertEquals(t, tun.sendCount, 0)
+	AssertNotRecvPacket(t, client1, "DTLS packet forwarded to other client")
+
+	var client2ID string
+	var client2Assoc AssociationID
+	for clientID := range mdd.clients {
+		if clientID != client1ID {
+			client2ID = clientID
+		}
+	}
+	client2Assoc, err = stringToAssoc(client2ID)
+	AssertNotError(t, err, "MDD provisioned an invalid client ID")
 
 	// Test RTP forwarding
-	c1, err := NewClient("c1", 9999, 8888)
-	AssertNotError(t, err, "Error creating client 1")
-	c1.Listen()
+	srtpPacket := []byte{128, 1, 0}
+	client1.Write(srtpPacket)
+	AssertRecvPacket(t, client2, srtpPacket, "SRTP packet not forwarded")
+	AssertNotRecvPacket(t, client1, "SRTP packet forwarded to sender")
 
-	c2, err := NewClient("c2", 7777, 8888)
-	AssertNotError(t, err, "Error creating client 2")
-	c2.Listen()
+	// Test DTLS to client 1
+	dtlsPacket := []byte{0x16, 0x01}
+	mdd.Send(client1Assoc, dtlsPacket)
+	AssertRecvPacket(t, client1, dtlsPacket, "DTLS packet was not forwarded")
+	AssertNotRecvPacket(t, client2, "DTLS packet forwarded to other client")
 
-	m1 := []byte("c1-init")
-	m2 := []byte("c2-probe")
-	m3 := []byte("c1-probe")
-
-	c1.Write(m1) // Primes c1 as a recipient
-	c2.Write(m2)
-	AssertRecvPacket(t, c1, m2, "Failed to forward c2->c1")
-	c1.Write(m3)
-	AssertRecvPacket(t, c2, m3, "Failed to forward c1->c2")
-
+	// Test DTLS to client 2
+	dtlsPacket = []byte{0x16, 0x01}
+	mdd.Send(client2Assoc, dtlsPacket)
+	AssertRecvPacket(t, client2, dtlsPacket, "DTLS packet was not forwarded")
+	AssertNotRecvPacket(t, client1, "DTLS packet forwarded to other client")
 }
