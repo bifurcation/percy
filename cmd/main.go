0f37fe9f773234e2edd71fb241da392a80deadff
Richard Barnes <rlb@ipv.sx>
2018-02-17 19:53:26 -0500

    Add an executable to run the MDD

---
+++ cmd/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	".."
+	"fmt"
+)
+
+type NoopTunnel bool
+
+func (tun NoopTunnel) Send(assoc percy.AssociationID, msg []byte) error {
+	return nil
+}
+
+func (tun NoopTunnel) SendWithProfiles(assoc percy.AssociationID, msg []byte, profiles []percy.ProtectionProfile) error {
+	return nil
+}
+
+func main() {
+	tunnel := NoopTunnel(false)
+	mdd := percy.NewMDD(tunnel)
+	err := mdd.Listen(4430)
+	if err != nil {
+		panic(err)
+	}
+	defer mdd.Stop()
+
+	fmt.Println("Listening, press <enter> to stop")
+
+	var input string
+	fmt.Scanln(&input)
+}
