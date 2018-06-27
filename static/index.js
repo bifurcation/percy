4317ea11e414d8081be5c77d2db37c224effb0f2
Nils Ohlmeier <github@ohlmeier.org>
2018-06-26 18:16:40 -0700

    One PeerConnection only, everything faked

--- static/index.js
+++ static/index.js
@@ -7,6 +7,8 @@ const RELAY_PORT = "RELAY_PORT_FROM_GO_SERVER";
 const IPV6_REGEX = RegExp('\:[0-9a-f]*\:[0-9a-fA-f]*','g');
 const TCP_REGEX = RegExp('.*tcptype.*','g');
 
+const ANSWER_SDP = {type: "answer", sdp: "v=0\r\no=percy0.1 2633292546686233323 0 IN IP4 0.0.0.0\r\ns=-\r\nt=0 0\r\na=fingerprint:sha-256 AA:AA:AA:AA:AA:AA:AA:AA:AA:AA:AA:AA:AA:AA:AA:AA:AA:AA:AA:AA:AA:AA:AA:AA:AA:AA:AA:AA:AA:AA:AA:AA\r\na=group:BUNDLE sdparta_0\r\na=ice-options:trickle\r\na=msid-semantic:WMS *\r\nm=video 9 UDP/TLS/RTP/SAVPF 120\r\nc=IN IP4 0.0.0.0\r\na=recvonly\r\na=extmap:3 urn:ietf:params:rtp-hdrext:sdes:mid\r\na=extmap:4 http://www.webrtc.org/experiments/rtp-hdrext/abs-send-time\r\na=extmap:5 urn:ietf:params:rtp-hdrext:toffset\r\na=fmtp:120 max-fs=12288;max-fr=60\r\na=ice-pwd:abcdefabcdefabcdefabcdefabcdefab\r\na=ice-ufrag:fedcbafe\r\na=mid:sdparta_0\r\na=rtcp-fb:120 nack\r\na=rtcp-fb:120 nack pli\r\na=rtcp-fb:120 ccm fir\r\na=rtcp-fb:120 goog-remb\r\na=rtcp-mux\r\na=rtpmap:120 VP8/90000\r\na=setup:passive\r\n"};
+
 // Handy element access
 let page = {
   get run() { return document.getElementById("run"); },
@@ -18,6 +20,7 @@ let page = {
   get remote() { return document.getElementById("remote"); },
 };
 
+
 function rewrite(c, host, port) {
   c.candidate = c.candidate.replace(IP_PORT_REGEX, `${host} ${port}`);
   return c;
@@ -25,7 +28,6 @@ function rewrite(c, host, port) {
 
 function run() {
   let offerer = new RTCPeerConnection();
-  let answerer = new RTCPeerConnection();
   
   console.log("wtf?");
 
@@ -36,52 +38,32 @@ function run() {
       offerer.addStream(stream);
     });
   
-  answerer.ontrack = e => {
-    console.log("got remote stream");
-    page.remote.srcObject = e.streams[0];
-  }
-  
   offerer.onicecandidate = e => {
-    if (!e.candidate ||
-        IPV6_REGEX.test(e.candidate.candidate) ||
-        TCP_REGEX.test(e.candidate.candidate)) {
-      console.log("Ignoring offerer candidate: " + JSON.stringify(e.candidate));
-      return;
-    }
-    console.log("got offerer ICE candidate: " + JSON.stringify(e.candidate));
-    let candidate = rewrite(e.candidate, RELAY_IP, RELAY_PORT);
-    page.offerICE.value += JSON.stringify(candidate, null, 2) + "\n\n";
-    console.log("adding offerer ICE candidate: " + JSON.stringify(candidate));
-    answerer.addIceCandidate(candidate);
+    console.log("dropping local ICE candidate: " + JSON.stringify(e.candidate));
+    return;
   }
 
-  answerer.onicecandidate = e => {
-    if (!e.candidate ||
-        IPV6_REGEX.test(e.candidate.candidate) ||
-        TCP_REGEX.test(e.candidate.candidate)) {
-      console.log("Ignoring answerer candidate: " + JSON.stringify(e.candidate));
-      return;
-    }
-    console.log("got answerer ICE candidate: " + JSON.stringify(e.candidate));
-    let candidate = rewrite(e.candidate, RELAY_IP, RELAY_PORT);
-    page.answerICE.value += JSON.stringify(candidate, null, 2) + "\n\n";
-    console.log("adding answerer ICE candidate: " + JSON.stringify(candidate));
-    offerer.addIceCandidate(candidate);
-  }
-
-  offerer.onnegotiationneeded = e =>
+  offerer.onnegotiationneeded = e => {
     offerer.createOffer().then(offer => {
       console.log("got offer");
       page.offer.value = offer.sdp;
-      offerer.setLocalDescription(offer);
-      answerer.setRemoteDescription(offer);
+      return offerer.setLocalDescription(offer);
     })
-    .then(() => answerer.createAnswer()).then(answer => {
-      console.log("got answer");
-      page.answer.value = answer.sdp;
-      answerer.setLocalDescription(answer);
-      offerer.setRemoteDescription(answer);
+    .then(() => {
+      console.log("setting static SDP answer");
+      page.answer.value = ANSWER_SDP.sdp;
+      return offerer.setRemoteDescription(ANSWER_SDP);
     })
+    .then(() => {
+      console.log("adding fake ICE candidates");
+      let c = {"candidate": "candidate:0 1 UDP 2122121471 " + RELAY_IP + " " + RELAY_PORT + " typ host","sdpMid": "sdparta_0","sdpMLineIndex": 0};
+      page.answerICE.value = JSON.stringify(c, null, 2) + "\n\n";
+      return offerer.addIceCandidate(c);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
 }
 
 window.onload = () => {
